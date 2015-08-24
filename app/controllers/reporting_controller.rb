3a10165333235e44c378beb30708c1177753596f
Octave <octave.auger@gmail.com>
2015-08-24 21:17:05 +0100

    Added layout

--- app/controllers/reporting_controller.rb
+++ app/controllers/reporting_controller.rb
@@ -5,10 +5,10 @@ class ReportingController < ApplicationController
   def payments
   	respond_to do |format|
   		format.html do
-  			@payments = Payment.includes(:events, mandate: { customer_bank_account: :customer }).all.paginate(page: params[:page])
+  			@payments = Payment.includes(:events, mandate: { customer_bank_account: :customer }).order('gc_created_at desc').all.paginate(page: params[:page])
   		end
   		format.csv do
-  			@payments = Payment.includes(:events, mandate: { customer_bank_account: :customer }).all
+  			@payments = Payment.includes(:events, mandate: { customer_bank_account: :customer }).order('gc_created_at desc').all
   			headers['Content-Disposition'] = "attachment; filename=\"payment-list.csv\""
   			headers['Content-Type'] ||= 'text/csv'
   		end
