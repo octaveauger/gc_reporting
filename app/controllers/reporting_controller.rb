db3d10ec57f3a84b8922a3d878c8c9f42cfde52e
Octave <octave.auger@gmail.com>
2015-09-06 02:23:07 +0100

    Added individual payout breakdown and CSV export

--- app/controllers/reporting_controller.rb
+++ app/controllers/reporting_controller.rb
@@ -11,7 +11,7 @@ class ReportingController < ApplicationController
     		end
     		format.csv do
     			@payments = Payment.includes(:events, mandate: { customer_bank_account: :customer }).order('gc_created_at desc').all
-    			headers['Content-Disposition'] = "attachment; filename=\"" + I18n.t('reporting.payments.csv_name') +".csv\""
+    			headers['Content-Disposition'] = "attachment; filename=\"" + I18n.t('reporting.payments.csv_name') + ".csv\""
     			headers['Content-Type'] ||= 'text/csv'
     		end
     	end
@@ -31,8 +31,27 @@ class ReportingController < ApplicationController
   end
 
   def payout
-    @payout = current_user.payouts.find_by(gc_id: params[:payout_gc_id])
-    redirect_to reporting_payouts_path, alert: 'This payout could not be found' if @payout.nil?
-    
+    begin
+      @payout = current_user.payouts.find_by(gc_id: params[:payout_gc_id])
+      if @payout.nil?
+        redirect_to reporting_payouts_path, alert: I18n.t('notices.payout_not_found')
+      else
+        @parent_event = @payout.events.where(action: 'paid').first
+
+        respond_to do |format|
+          format.html do
+            @events = Event.where(parent_event_id: @parent_event.gc_id).all.paginate(page: params[:page])
+          end
+          format.csv do
+            @events = Event.where(parent_event_id: @parent_event.gc_id).all
+            headers['Content-Disposition'] = "attachment; filename=\"" + I18n.t('reporting.payout.csv_name') + "-" + @payout.gc_id + ".csv\""
+            headers['Content-Type'] ||= 'text/csv'
+          end
+        end
+      end
+    rescue => e
+      Utility.log_exception e
+      flash[:alert] = I18n.t('errors.exceptions.default')
+    end
   end
 end
