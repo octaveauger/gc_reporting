f2081c42d41f4cd5e85da1331996e46a8ea75b8c
Octave <octave.auger@gmail.com>
2015-09-17 22:35:00 +0100

    Added mandates list

--- app/controllers/reporting_controller.rb
+++ app/controllers/reporting_controller.rb
@@ -1,6 +1,24 @@
 class ReportingController < ApplicationController
   def index
-    redirect_to reporting_payments_path
+    redirect_to reporting_mandates_path
+  end
+
+  def mandates
+    begin
+      respond_to do |format|
+        format.html do
+          @mandates = Mandate.includes(:customer_bank_account, { customer_bank_account: :customer }).order('gc_created_at desc').all.paginate(page: params[:page])
+        end
+        format.csv do
+          @mandates = Mandate.includes(:customer_bank_account, { customer_bank_account: :customer }).order('gc_created_at desc').all
+          headers['Content-Disposition'] = "attachment; filename=\"" + I18n.t('reporting.mandates.csv_name') + ".csv\""
+          headers['Content-Type'] ||= 'text/csv'
+        end
+      end
+    rescue => e
+      Utility.log_exception e
+      flash[:alert] = I18n.t('errors.exceptions.default')
+    end
   end
 
   def payments
