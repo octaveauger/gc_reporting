c7221fd50145bb7b3493d651f96794933a97bab4
Octave <octave.auger@gmail.com>
2015-11-03 00:38:06 +0000

    Client list

---
+++ app/controllers/clients_controller.rb
@@ -0,0 +1,41 @@
+class ClientsController < ApplicationController
+  before_action :logged_in_user
+
+  def index
+    begin
+      params_filters = params.slice(:time_filter)
+      @time_filter = params[:time_filter] || 'any'
+      respond_to do |format|
+        format.html do
+          @clients = current_user.clients.filter(params_filters).includes(:customers, :mandates).order('created_at desc').all.paginate(page: params[:page])
+        end
+        format.js do
+          @clients = current_user.clients.filter(params_filters).includes(:customers, :mandates).order('created_at desc').all.paginate(page: params[:page])
+        end
+        format.csv do
+          @clients = current_user.clients.filter(params_filters).includes(:customers, :mandates).order('created_at desc').all
+          headers['Content-Disposition'] = "attachment; filename=\"" + I18n.t('clients.index.csv_name') + ".csv\""
+          headers['Content-Type'] ||= 'text/csv'
+        end
+      end
+    rescue => e
+      Utility.log_exception e
+      flash[:alert] = I18n.t('errors.exceptions.default')
+    end
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,18 @@
 class Client < ActiveRecord::Base
   include Tokenable
+  include Filterable
 
   belongs_to :client_source
   belongs_to :organisation
   has_many :customers
+  has_many :customer_bank_accounts, through: :customers
+  has_many :mandates, through: :customer_bank_accounts
+  has_many :payments, through: :mandates
+  has_many :events, through: :payments
+  has_many :refunds, through: :payments
+  has_many :subscriptions, through: :mandates
+
+  def full_name
+  	self.fname.to_s + ' ' + self.lname.to_s
+  end
 end
