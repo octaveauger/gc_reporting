38e95d9ad52d7ac64116183435300dbcd3a2f6e2
Octave <octave.auger@gmail.com>
2015-11-05 00:21:16 +0000

    Fixed infinite scrolling for clients index

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -15,6 +15,18 @@ $(function () {
   	e.preventDefault();
   	$.getScript($(this).attr('data-target'));
   });
+
+  // Infinite scrolling
+  if($('#infinite-scrolling').size() > 0) {
+    $(window).on('scroll', function(e) {
+      more_url = $('.pagination .next_page a').attr('href');
+      if(more_url && $(window).scrollTop() > $(document).height() - $(window).height() - 180) {
+        $('.pagination').html('<img src="/assets/ajax-loader.gif" alt="Loading..." title="Loading..." />');
+        $.getScript(more_url);
+      }
+    });
+    $(window).scroll(); // Triggers it at page load in case it's not below the fold
+  }
 });
 
 function autorefresh_sync_loader() {
