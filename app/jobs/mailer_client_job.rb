6e1c56dbbe6201c85d9316532f18f8c9bdaca09f
Octave <octave.auger@gmail.com>
2015-11-06 00:26:03 +0000

    Fixing mailer client job + larger email display

--- app/jobs/mailer_client_job.rb
+++ app/jobs/mailer_client_job.rb
@@ -3,7 +3,9 @@ class MailerClientJob
   workers 2
     
   def perform(client, email_type)
-  	#OrgClientMailer.method(email_type).call(client).deliver
-  	OrgClientMailer.mandate_request(client).deliver
+  	case email_type # we can't use OrgClientMailer.method(email_type).call(client).deliver - see http://stackoverflow.com/questions/17088619/how-can-you-call-class-methods-on-mailers-when-theyre-not-defined-as-such
+  	when 'mandate_request'
+  		OrgClientMailer.mandate_request(client).deliver
+  	end
   end
 end
